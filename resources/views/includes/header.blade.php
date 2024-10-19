904744adb8ca237c7fccbd4edcb2c959b35cc8e7
vahnmarty <vahnmarty@gmail.com>
2024-10-19 14:07:20 +0800

    initial commits

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+<header class="py-4 bg-white border-b border-gray-300">
+    <div class="px-8 py-6 mx-auto max-w-7xl">
+        <div class="flex items-center justify-between">
+            <a href="">
+                <img src="{{  asset('img/logo.png') }}" alt="" class="w-auto h-10">
+            </a>
+            <div class="flex gap-6">
+                <a href="" class="text-md text-primary-green">About</a>
+                <a href="" class="text-md text-primary-green">Blog</a>
+                <a href="" class="text-md text-primary-green">Contact</a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/sample.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>IFC Bootcamp</title>
+
+</head>
+<body>
+    <section class="section" id="hero">
+        <div>
+            <h1 id="site-header">Hello World</h1>
+            <p class="paragraph">Welcome to IFC Bootcamp</p>
+        </div>
+    </section>
+</body>
+</html>
